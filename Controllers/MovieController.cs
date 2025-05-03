73fd81090224b3e1dc44bfe88f6efd42108fbff0
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-05-03 17:40:07 +0700

    update dikit

--- Controllers/MovieController.cs
+++ Controllers/MovieController.cs
@@ -9,9 +9,9 @@ namespace modul10_103022300148.Controllers
     {
         private static readonly List<Movie> movies = new()
         {
-            new Movie("The Shawshank Redemption","Frank Darabont", new List<string>{"MetaScore : 82","IMDb Rating : 9.3" },"A banker convicted of uxoricide forms a friendship over a quarter century with a hardened convict, while maintaining his innocence and trying to remain hopeful through simple compassion."),
-            new Movie("The Godfather","Francis Ford Copolla", new List<string>{"MetaScore : 100","IMDb Rating : 9.2" },"The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son."),
-            new Movie("The Dark Knight","Christoper Nolan", new List<string>{"MetaScore : 84","IMDb Rating : 9.0" },"When a menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman, James Gordon and Harvey Dent must work together to put an end to the madness.")
+            new Movie("The Shawshank Redemption","Frank Darabont", new List<string>{"Tim Robbins","Morgan Freeman","Bob Gunton" },"A banker convicted of uxoricide forms a friendship over a quarter century with a hardened convict, while maintaining his innocence and trying to remain hopeful through simple compassion."),
+            new Movie("The Godfather","Francis Ford Copolla", new List<string>{"Marlon Brandon","Al Pacino","James Caan" },"The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son."),
+            new Movie("The Dark Knight","Christoper Nolan", new List<string>{"Christian Bale","Heath Ledger","Aaron Eckhart" },"When a menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman, James Gordon and Harvey Dent must work together to put an end to the madness.")
         };
 
         [HttpGet]
